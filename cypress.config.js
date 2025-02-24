b174ce0311fb632d7ff7799abaffa42ced6b7491
dhananjaylavate <dhananjaylavate1995@gmail.com>
2025-02-24 15:46:05 +0000

    removing-comments

--- cypress.config.js
+++ cypress.config.js
@@ -3,26 +3,20 @@ const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
-// const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
-// const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
-// const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
-
-const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
-const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
-const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
-
 module.exports = defineConfig({
   e2e: {
-    async setupNodeEvents(on, config) {
+   async setupNodeEvents(on, config) {
+      // implement node event listeners here
+      
       const bundler = createBundler({
-        plugins: [createEsbuildPlugin(config)],
+        plugins: [createEsbuildPlugin(config)],  // Ensure this is correctly configured
       });
 
-      on("file:preprocessor", bundler);
+      on('file:preprocessor', bundler);
       await addCucumberPreprocessorPlugin(on, config);
 
       return config;
     },
-    specPattern: "cypress/e2e/**/*.feature",
+    specPattern: "cypress/e2e/**/*.feature"
   },
 });
--- cypress/e2e/features/login.feature
+++
@@ -1,7 +0,0 @@
-# cypress/e2e/login.feature
-Feature: Login functionality
-
-  Scenario: Successful login
-    Given I am on the login page
-    When I enter valid credentials
-    Then I should be redirected to the dashboard
