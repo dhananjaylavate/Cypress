ddd7618afa79fab3fe29c8eb96433f78d4aea794
dhananjaylavate <dhananjaylavate1995@gmail.com>
2025-02-24 16:15:59 +0000

    faking-names

--- cypress/e2e/features/registration/registration.js
+++ cypress/e2e/features/registration/registration.js
@@ -1,5 +1,6 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { REGISTRATION_PAGE_ELEMENT } from "cypress/e2e/elements/registration";
+import { faker } from "@faker-js/faker";
 
 Given('I visit the Registration page', function (){
     cy.visit('https://parabank.parasoft.com/parabank');
@@ -7,10 +8,16 @@ Given('I visit the Registration page', function (){
 
 When("User enter Registration details", function (){
     
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+
+    cy.writeFile('cypress/fixtures/firstNames.json', { firstName }, 'utf8');
+
+
     cy.clickButton(REGISTRATION_PAGE_ELEMENT.registrationLink)
 
-    cy.getElement(REGISTRATION_PAGE_ELEMENT.firstName).type('Dhananjay');
-    cy.getElement(REGISTRATION_PAGE_ELEMENT.lastName).type('Lavate');
+    cy.getElement(REGISTRATION_PAGE_ELEMENT.firstName).type(firstName);
+    cy.getElement(REGISTRATION_PAGE_ELEMENT.lastName).type(lastName);
 
     cy.getElement(REGISTRATION_PAGE_ELEMENT.address).type('Solapur');
     cy.getElement(REGISTRATION_PAGE_ELEMENT.city).type('solapur');
@@ -21,7 +28,7 @@ When("User enter Registration details", function (){
     cy.getElement(REGISTRATION_PAGE_ELEMENT.ssn).type('abcd');
 
 
-    cy.getElement(REGISTRATION_PAGE_ELEMENT.userName).type('Dhananjay');
+    cy.getElement(REGISTRATION_PAGE_ELEMENT.userName).type(firstName);
     cy.getElement(REGISTRATION_PAGE_ELEMENT.passWord).type('abcd');
     cy.getElement(REGISTRATION_PAGE_ELEMENT.confirmPassword).type('abcd');
     
@@ -29,6 +36,6 @@ When("User enter Registration details", function (){
 
 Then("I click the Registration button", function(){
     cy.clickButton(REGISTRATION_PAGE_ELEMENT.registorBtn)
-    cy.getElement(REGISTRATION_PAGE_ELEMENT.duplicateUSerMsg).contains('This username already exists')
+    //cy.getElement(REGISTRATION_PAGE_ELEMENT.duplicateUSerMsg).contains('This username already exists')
      
 });
